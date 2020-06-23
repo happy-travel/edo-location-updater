5ad5d2735abe19224c54b8e22288ec26740aa6b8
Vadim Mingazhev <vadim.mingazhev@gmail.com>
2020-06-24 00:00:40 +0500

    Moved getting token to separate configurable singleton service

--- HappyTravel.LocationUpdater/Infrastructure/ProtectedApiBearerTokenHandler.cs
+++ HappyTravel.LocationUpdater/Infrastructure/ProtectedApiBearerTokenHandler.cs
@@ -1,18 +1,15 @@
-using System;
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 using IdentityModel.Client;
-using Microsoft.Extensions.Options;
 
 namespace HappyTravel.LocationUpdater.Infrastructure
 {
     public class ProtectedApiBearerTokenHandler : DelegatingHandler
     {
-        public ProtectedApiBearerTokenHandler(IHttpClientFactory clientFactory, IOptions<TokenRequest> tokenRequest)
+        public ProtectedApiBearerTokenHandler(TokenProvider tokenProvider)
         {
-            _clientFactory = clientFactory;
-            _tokenRequest = tokenRequest.Value;
+            _tokenProvider = tokenProvider;
         }
 
 
@@ -20,47 +17,10 @@ namespace HappyTravel.LocationUpdater.Infrastructure
             HttpRequestMessage request,
             CancellationToken cancellationToken)
         {
-            request.SetBearerToken(await GetToken());
+            request.SetBearerToken(await _tokenProvider.GetToken());
             return await base.SendAsync(request, cancellationToken);
         }
-
-
-        private async Task<string> GetToken()
-        {
-            await TokenSemaphore.WaitAsync();
-            var dateNow = DateTime.UtcNow;
-            // We need to cache token because users can send several requests in short periods.
-            // Covered situation when after checking expireDate token will expire immediately.
-            if (!_tokenInfo.Equals(default) && (_tokenInfo.ExpiryDate - dateNow).TotalSeconds >= 5)
-            {
-                TokenSemaphore.Release();
-                return _tokenInfo.Token;
-            }
-
-            using var client = _clientFactory.CreateClient(HttpClientNames.Identity);
-            var clientCredentialsTokenRequest = new ClientCredentialsTokenRequest
-            {
-                Address = _tokenRequest.Address,
-                ClientId = _tokenRequest.ClientId,
-                ClientSecret = _tokenRequest.ClientSecret,
-                Scope = "edo"
-            };
-
-            var tokenResponse = await client.RequestClientCredentialsTokenAsync(clientCredentialsTokenRequest);
-            if (tokenResponse.IsError)
-                throw new HttpRequestException(
-                    $"Something went wrong while requesting the access token For Connectors. Error: {tokenResponse.Error}");
-
-            _tokenInfo = (tokenResponse.AccessToken, dateNow.AddSeconds(tokenResponse.ExpiresIn));
-            TokenSemaphore.Release();
-            return _tokenInfo.Token;
-        }
-
-
-        private readonly IHttpClientFactory _clientFactory;
-        private readonly TokenRequest _tokenRequest;
-
-        private static (string Token, DateTime ExpiryDate) _tokenInfo;
-        private static readonly SemaphoreSlim TokenSemaphore = new SemaphoreSlim(1, 1);
+        
+        private readonly TokenProvider _tokenProvider;
     }
 }
